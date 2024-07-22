fab0a4cbcbf26b4125046dccabe1348d92b81fd9
Reness <reness9910@gmail.com>
2024-07-22 14:29:53 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -3,10 +3,11 @@
 set -x
 # Remove existing container if it exists
 docker rm -f my-flask-app:latest || true
-docker run -d -p 80:80 --name my-flask-app -v "/mnt/c/mods/y2t3/ssd/own github/flaskdemo" my-flask-app:latest
+#docker run -d -p 5000:5000 --name my-flask-app -v "/mnt/c/mods/y2t3/ssd/own github/flaskdemo" my-flask-app:latest
+docker run -d -p 0.0.0.0:5000:5000 --name my-flask-app -v "/mnt/c/mods/y2t3/ssd/own github/flaskdemo" my-flask-app:latest
 
 sleep 1
 set +x
 
 echo 'Now...'
-echo 'Visit http://localhost:5000 to see your Flask application in action.'
+echo 'Visit http://192.168.1.9:5000 to see your Flask application in action.'
