dfa9f21f89fe75c9946a81e92996ca3ecccc348a
Reness <reness9910@gmail.com>
2024-07-22 11:30:04 +0800

    add separate dockerfiles

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -2,10 +2,10 @@
 
 set -x
 # Remove existing container if it exists
-docker rm -f my-apache-php-app || true
-docker run -d -p 80:80 --name my-apache-php-app -v /mnt/c/Mods/Y2T3/SSD/Own Github/FlaskDemo/src:/var/www/html php:7.2-apache
+docker rm -f my-flask-app || true
+docker run -d -p 5000:5000 --name my-flask-app -v /mnt/c/Mods/Y2T3/SSD/Own Github/FlaskDemo:/app my-jenkins-flask-app
 sleep 1
 set +x
 
 echo 'Now...'
-echo 'Visit http://localhost to see your PHP application in action.'
+echo 'Visit http://localhost:5000 to see your Flask application in action.'
